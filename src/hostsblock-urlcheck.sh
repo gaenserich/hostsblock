094b51332651ae8a1e29fea386a435f546307a70
Jacob van der Kolk <jav209@psu.edu>
2015-06-11 08:58:55 -0400

    added more verbosity logic to hostsblock-urlcheck.sh

--- src/hostsblock-urlcheck.sh
+++ src/hostsblock-urlcheck.sh
@@ -47,7 +47,13 @@ _set_subprocess_verbosity
 _changed=0
 echo "Checking to see if url is blocked or unblocked..."
 _check_url $(echo "$@" | sed -e "s/.*https*:\/\///g" -e "s/[\/?'\" :<>\(\)].*//g")
-[ $_changed == 1 ] && postprocess &>/dev/null
+if [ $_changed == 1 ]; then
+    if [ $verbosity -ge 5 ]; then
+        postprocess
+    else
+        postprocess &>/dev/null
+    fi
+fi
 read -p "Page domain verified. Scan the whole page for other domains for (un)blocking? [y/N] " a
 if [[ $a == "y" || $a == "Y" ]]; then
     for LINE in `curl -L --location-trusted -s "$@" | tr ' ' '\n' | grep "https*:\/\/" | sed -e "s/.*https*:\/\/\(.*\)$/\1/g" -e "s/\//\n/g" | grep "\." |\
