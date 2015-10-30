bf6038e35a5e100b5c56d2848c474b0f99b40058
Jacob van der Kolk <jav209@psu.edu>
2015-10-30 08:54:02 -0400

    Added more optional source files to hostsblock.conf. Added user agent info to curl to make work with more sites. Sanitized hostsblock.conf file a little bit.

--- src/hostsblock.sh
+++ src/hostsblock.sh
@@ -77,7 +77,7 @@ for _url in ${blocklists[*]}; do
         _notify 4 "Cache file $cachedir/$_outfile for blocklist $_url not found. It will be downloaded."
     fi
     _notify 4 "Checking and, if needed, downloading blocklist $_url to $cachedir/$_outfile"
-    if curl $_v_curl --compressed -L --connect-timeout $connect_timeout --retry $retry -z "$cachedir"/"$_outfile" "$_url" -o "$cachedir"/"$_outfile"; then
+    if curl -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ $_v_curl --compressed -L --connect-timeout $connect_timeout --retry $retry -z "$cachedir"/"$_outfile" "$_url" -o "$cachedir"/"$_outfile"; then
         _notify 3 "Refreshed blocklist $_url."
         _new_ls=$(ls -l "$cachedir"/"$_outfile")
         if [ "$_old_ls" != "$_new_ls" ]; then
