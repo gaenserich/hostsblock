934148b55518023ab13c7cc77b9fb99f3ee3158a
Jake van der Kolk <jakevanderkolk@gmail.com>
2018-07-08 12:39:19 -0400

    Fixed a couple more things.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ DESTDIR="/usr/bin"
 _destdir_ok=0
 until [ $_destdir_ok -eq 1 ]; do
     msg "Destination directory for hostsblock is $DESTDIR. Enter a new path or press Enter to keep as is."
-    read -rp "$DESTDIR " d
+    read -rp "[$DESTDIR] " d
     if echo "$d" | grep -q "\\/$"; then
         DESTDIR="${d%/}"
     elif [ "$d" != "" ]; then
@@ -56,10 +56,10 @@ if getent passwd | grep -q "^hostsblock:"; then
     echo "Using preexisting user 'hostsblock'"
     _homedir_ok=0
     _homedir_changed=0
-    HOMEDIR=$(getent passwd | grep "^hostsblock:" | cut -d':' -f6)
+    HOMEDIR=$(getent passwd hostsblock | cut -d':' -f6)
     until [ $_homedir_ok -eq 1 ]; do
         msg "User 'hostsblock' has home directory $HOMEDIR. Enter a new path or press Enter to keep as is."
-        read -rp "$HOMEDIR " h
+        read -rp "[$HOMEDIR] " h
         if echo "$h" | grep -q "\\/$"; then
             HOMEDIR="${h%/}"
             _homedir_changed=1
@@ -92,7 +92,7 @@ if getent passwd | grep -q "^hostsblock:"; then
     done
     if [ $_homedir_changed -eq 1 ]; then
         n="y"
-        msg "Should the content from the previous home directory be moved to this new directory?" 
+        msg "Should the content from the previous home directory be moved to this new directory?"
         read -rp "[Y/n] " n
         if [ "$n" == "n" ] || [ "$n" == "N" ]; then
             usermod -d "$HOMEDIR" -m hostsblock
@@ -100,7 +100,7 @@ if getent passwd | grep -q "^hostsblock:"; then
             usermod -d "$HOMEDIR" hostsblock
         fi
     fi
-    if getent group | grep -q "^hostsblock:"; then
+    if getent group hostsblock; then
         msg "Using preexisting group 'hostsblock'"
     else
         msg "Creating group 'hostsblock'..."
@@ -145,12 +145,16 @@ gpasswd -A hostsblock hostsblock
 
 if pidof dnsmasq; then
     dnsmasq_user=$(ps -o user= -p "$(pidof dnsmasq)")
-    msg "You appear to be running dnsmasq under user $dnsmasq_user. If you will be using hostsblock with dnsmasq as a caching daemon, dnsmasq needs read access to hostsblock's home directory. To do so, should I add $dnsmasq_user to the hostblock group?"
-    e="n"
-    read -rp "[y/N] " e
-    if [ "$e" == "y" ] || [ "$e" == "Y" ]; then
-        gpasswd -a "$dnsmasq_user" hostsblock
+    if getent group hostsblock | cut -d":" -f4 | tr ',' '\n' | grep -q "^${dnsmasq_user}$"; then
         gpasswd -M "$dnsmasq_user" hostsblock
+    else
+        msg "You appear to be running dnsmasq under user $dnsmasq_user. If you will be using hostsblock with dnsmasq as a caching daemon, dnsmasq needs read access to hostsblock's home directory. To do so, should I add $dnsmasq_user to the hostblock group?"
+        e="n"
+        read -rp "[y/N] " e
+        if [ "$e" == "y" ] || [ "$e" == "Y" ]; then
+            gpasswd -a "$dnsmasq_user" hostsblock
+            gpasswd -M "$dnsmasq_user" hostsblock
+        fi
     fi
 else
     msg "If you are using hostsblock with a dns cacher, you should add the user under which the cacher runs to the 'hostsblock' group so that the daemon can access your generated host file. Enter the user of the DNS daemon or leave blank to add no additional user."
@@ -163,7 +167,7 @@ fi
 
 msg "In order to manage hostsblock correctly, you must run the script as the user 'hostsblock', even when using the 'hostsblock-urlcheck' script (aka 'hostsblock -c'). To do so, type 'sudo -u hostsblock hostsblock' or 'sudo -u hostsblock hostsblock-urlcheck', etc. Before you can do this, however, the following line must be added to sudoers:"
 printf "\\n"
-msg "jake	ALL	=	(hostsblock)	NOPASSWD: $DESTDIR/hostsblock,$DESTDIR/hostsblock-urlcheck"
+echo "jake	ALL	=	(hostsblock)	NOPASSWD: $DESTDIR/hostsblock,$DESTDIR/hostsblock-urlcheck"
 printf "\\n"
 msg "where 'jake' is the user from which you want to manage hostsblock. Do you want to add this line to the bottom of sudoers right now? (if so, make sure to copy the text now)."
 read -rp "[y/N] " dosu
@@ -201,6 +205,7 @@ while [ ${#_dir} -gt 0 ]; do
     else
         chmod o+x "$_dir"
     fi
+    _dir="${_dir%/*}"
 done
 
 msg "Should I enable and/or start the hostsblock service? (Requires systemd)"
